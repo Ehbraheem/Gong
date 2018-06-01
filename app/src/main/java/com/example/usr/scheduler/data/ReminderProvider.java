ae2592771d8ff5a85627380e3fd201b9ad7760cf
Ehbraheem <ehbraheem@gmail.com>
2018-06-01 18:06:53 +0100

    Add Uri logic for matches

--- app/src/main/java/com/example/usr/scheduler/data/ReminderProvider.java
+++ app/src/main/java/com/example/usr/scheduler/data/ReminderProvider.java
@@ -2,14 +2,36 @@ package com.example.usr.scheduler.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
+import static com.example.usr.scheduler.data.ReminderContract.CONTENT_AUTHORITY;
+import static com.example.usr.scheduler.data.ReminderContract.TABLE_NAME;
+
 public class ReminderProvider extends ContentProvider {
+
+    private ReminderSQLite mReminderSQLite;
+
+    /** Matcher identifier for all reminder **/
+    private static final int REMINDERS = 100;
+    /** Matcher identifier for one reminder **/
+    private static final int SINGLE_REMINDER = 101;
+
+    private static final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        // "content://com.example.usr.scheduler/reminders"
+        matcher.addURI(CONTENT_AUTHORITY, TABLE_NAME, REMINDERS);
+
+        // "content://com.example.usr.scheduler/reminders"
+        matcher.addURI(CONTENT_AUTHORITY, TABLE_NAME + "/#", SINGLE_REMINDER);
+    }
 
     @Override
     public boolean onCreate() {
-        return false;
+        mReminderSQLite = new ReminderSQLite(getContext());
+        return true;
     }
 
     @Override
