727babbafa035d1081e198ddeaeff380f0ae09c8
Ehbraheem <ehbraheem@gmail.com>
2018-06-01 16:53:00 +0100

    Add Contract for DB

---
+++ app/src/main/java/com/example/usr/scheduler/data/ReminderContract.java
@@ -0,0 +1,15 @@
+package com.example.usr.scheduler.data;
+
+public final class ReminderContract {
+
+    public static final String TABLE_NAME = "reminders";
+
+    public static final String REMINDER_TITLE = "title";
+
+    public static final String REMINDER_DATE = "date";
+
+    public static final String REMINDER_TYPE = "type";
+
+    public static final String REMINDER_BODY = "body";
+
+}
