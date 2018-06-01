afef2fe0ade62a6cdd020546d9702b5314d5009d
Ehbraheem <ehbraheem@gmail.com>
2018-06-01 17:26:30 +0100

    Add Reminder DB Helper

---
+++ app/src/main/java/com/example/usr/scheduler/data/ReminderSQLite.java
@@ -0,0 +1,44 @@
+package com.example.usr.scheduler.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.usr.scheduler.data.ReminderContract.REMINDER_BODY;
+import static com.example.usr.scheduler.data.ReminderContract.REMINDER_DATE;
+import static com.example.usr.scheduler.data.ReminderContract.REMINDER_TITLE;
+import static com.example.usr.scheduler.data.ReminderContract.REMINDER_TYPE;
+import static com.example.usr.scheduler.data.ReminderContract.TABLE_NAME;
+import static com.example.usr.scheduler.data.ReminderContract._ID;
+
+public class ReminderSQLite extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "reminder.db";
+    public static final int DB_VERSION = 0;
+
+    public ReminderSQLite(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        final String SQL_CREATE = "CREATE TABLE " + TABLE_NAME + " ( "   +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "             +
+                REMINDER_TITLE + " STRING NOT NULL, "                    +
+                REMINDER_DATE + " TIMESTAMP NOT NULL, "                  +
+                REMINDER_TYPE + " STRING NOT NULL, "                     +
+                REMINDER_BODY + " TEXT NOT NULL, "                       +
+                "UNIQUE ( " + REMINDER_TITLE + " ) ON CONFLICT REPLACE " +
+                " );";
+
+        sqLiteDatabase.execSQL(SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        onCreate(db);
+    }
+}
