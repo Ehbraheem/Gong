f5674daeea74a876ae3587edb2cf8e15f6863feb
Ehbraheem <ehbraheem@gmail.com>
2018-06-10 18:28:39 +0100

    Wire Database to facilitate dummy data creation onStart

--- app/src/main/java/com/example/usr/scheduler/data/ReminderSQLite.java
+++ app/src/main/java/com/example/usr/scheduler/data/ReminderSQLite.java
@@ -1,8 +1,22 @@
 package com.example.usr.scheduler.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.usr.scheduler.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import static com.example.usr.scheduler.data.ReminderContract.Columns.REMINDER_BODY;
 import static com.example.usr.scheduler.data.ReminderContract.Columns.REMINDER_DATE;
@@ -16,8 +30,13 @@ public class ReminderSQLite extends SQLiteOpenHelper {
     public static final String DB_NAME = "reminder.db";
     public static final int DB_VERSION = 0;
 
+    public static final String TAG = ReminderSQLite.class.getName();
+
+    private Resources mResources;
+
     public ReminderSQLite(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        mResources = context.getResources();
     }
 
     @Override
@@ -33,6 +52,8 @@ public class ReminderSQLite extends SQLiteOpenHelper {
                 " );";
 
         sqLiteDatabase.execSQL(SQL_CREATE);
+
+        addDemoReminders(sqLiteDatabase);
     }
 
     @Override
@@ -40,5 +61,66 @@ public class ReminderSQLite extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 
         onCreate(db);
+    }
+
+    private void addDemoReminders(SQLiteDatabase db) {
+        try {
+            db.beginTransaction();
+            try {
+                readRemindersFromResource(db);
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+            }
+        } catch (IOException | JSONException e) {
+            Log.e(TAG, "Unable to pre-fill database", e);
+        }
+    }
+
+    private void readRemindersFromResource(SQLiteDatabase db) throws IOException, JSONException {
+        StringBuilder builder = new StringBuilder();
+        InputStream in = mResources.openRawResource(R.raw.reminders);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+
+        // Parse resource to key/values
+        JSONObject root = new JSONObject(builder.toString());
+
+        JSONArray reminders = getReminders(root);
+        
+        // Inserts values to Database
+        insertRemindersFromJSONArray(db, reminders);
+    }
+
+    private void insertRemindersFromJSONArray(SQLiteDatabase db, JSONArray reminders) throws JSONException {
+        for (int i = 0; i < reminders.length(); i++) {
+            JSONObject reminder = reminders.getJSONObject(i);
+            insertReminder(reminder, db);
+        }
+    }
+
+    private void insertReminder(JSONObject reminder, SQLiteDatabase db) throws JSONException {
+        String title = reminder.getString("title");
+        String date = reminder.getString("date");
+        String type = reminder.getString("type");
+        String body = reminder.getString("body");
+
+        ContentValues values = new ContentValues();
+        values.put("title", title);
+        values.put("date", date);
+        values.put("body", body);
+        values.put("type", type);
+
+        db.insert(ReminderContract.TABLE_NAME,
+                null,
+                values);
+    }
+
+    private JSONArray getReminders(JSONObject root) throws JSONException {
+        return root.getJSONArray("reminders");
     }
 }
